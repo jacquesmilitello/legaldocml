03a05572d56932c36463c5349a4ebf829de72637
Jacques Militello <jacques.militello@gmail.com>
2017-08-12 14:36:22 +0200

    add test for sonar

---
+++ legaldocml-mod-xml/src/test/java/io/legaldocml/mod/xml/XmlModuleTest.java
@@ -0,0 +1,11 @@
+package io.legaldocml.mod.xml;
+
+import org.junit.Test;
+
+public class XmlModuleTest {
+
+    @Test
+    public void test() {
+
+    }
+}
---
+++ legaldocml-mod-xsi/src/test/java/io/legaldocml/mod/xsi/XsiModuleTest.java
@@ -0,0 +1,11 @@
+package io.legaldocml.mod.xsi;
+
+import org.junit.Test;
+
+public class XsiModuleTest {
+
+    @Test
+    public void test() {
+
+    }
+}
