926cf47e70c11b6c90193fe439513bb6265ecc5e
Jacques Militello <jacques.militello@gmail.com>
2017-09-08 11:04:46 +0200

    add toString

--- legaldocml-core/src/main/java/io/legaldocml/io/impl/DefaultXmlProvider.java
+++ legaldocml-core/src/main/java/io/legaldocml/io/impl/DefaultXmlProvider.java
@@ -5,6 +5,7 @@ import io.legaldocml.io.XmlReaderFactory;
 import io.legaldocml.io.XmlReaderFactoryProvider;
 import io.legaldocml.io.XmlWriterFactory;
 import io.legaldocml.io.XmlWriterFactoryProvider;
+import io.legaldocml.util.ToStringBuilder;
 
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
@@ -15,13 +16,31 @@ public final class DefaultXmlProvider extends XmlProvider {
 
     private final XmlReaderFactory readerFactory = XmlReaderFactoryProvider.newXmlReaderFactory(16);
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected XmlWriterFactory getWriterFactory() {
         return writerFactory;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected XmlReaderFactory getReaderFactory() {
         return readerFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(false)
+                .append("class",getClass().getSimpleName())
+                .append("writerFactory.size",16)
+                .append("readerFactory.size",16)
+                .toString();
     }
 }
