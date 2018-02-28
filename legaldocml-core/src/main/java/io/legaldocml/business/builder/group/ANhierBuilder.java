fea884d6d7907c07a383580d74287525a3add5a4
charboubm <mustapha.charboub@cgi.com>
2018-02-28 17:26:16 +0100

    add alinea support class with tests

--- legaldocml-core/src/main/java/io/legaldocml/business/builder/group/ANhierBuilder.java
+++ legaldocml-core/src/main/java/io/legaldocml/business/builder/group/ANhierBuilder.java
@@ -2,18 +2,12 @@ package io.legaldocml.business.builder.group;
 
 import io.legaldocml.akn.AknObject;
 import io.legaldocml.akn.container.ANhierContainer;
-import io.legaldocml.business.builder.support.ArticleSupport;
-import io.legaldocml.business.builder.support.ChapterSupport;
-import io.legaldocml.business.builder.support.ListSupport;
-import io.legaldocml.business.builder.support.ParagraphSupport;
-import io.legaldocml.business.builder.support.PointSupport;
-import io.legaldocml.business.builder.support.SectionSupport;
-import io.legaldocml.business.builder.support.SubSectionSupport;
+import io.legaldocml.business.builder.support.*;
 
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
  */
-public interface ANhierBuilder<T extends ANhierContainer<E>, E extends AknObject> extends ChapterSupport<T,E>, SectionSupport<T,E>, ParagraphSupport<T,E>,
-        ArticleSupport<T,E>, SubSectionSupport<T,E>, ListSupport<T,E>, PointSupport<T,E> {
+public interface ANhierBuilder<T extends ANhierContainer<E>, E extends AknObject> extends ChapterSupport<T, E>, SectionSupport<T, E>, ParagraphSupport<T, E>,
+        ArticleSupport<T, E>, SubSectionSupport<T, E>, ListSupport<T, E>, AlineaSupport<T, E>, PointSupport<T, E> {
 
 }
---
+++ legaldocml-core/src/main/java/io/legaldocml/business/builder/support/AlineaSupport.java
@@ -0,0 +1,27 @@
+package io.legaldocml.business.builder.support;
+
+import io.legaldocml.akn.AknObject;
+import io.legaldocml.akn.container.ANhierContainer;
+import io.legaldocml.akn.element.Alinea;
+import io.legaldocml.business.builder.element.HierarchyBuilder;
+
+import java.util.function.Consumer;
+
+/**
+ * @author <a href="mailto:mustapha.charboub@gmail.com">Mustapha CHARBPUB</a>
+ */
+public interface AlineaSupport<T extends ANhierContainer<E>, E extends AknObject> extends SupportBuilder<T> {
+
+    default HierarchyBuilder<Alinea> alinea() {
+        return alinea(null);
+    }
+
+    default HierarchyBuilder<Alinea> alinea(Consumer<Alinea> consumer) {
+        Alinea alinea = new Alinea();
+        parent().add(alinea);
+        if (consumer != null) {
+            consumer.accept(alinea);
+        }
+        return new HierarchyBuilder<>(businessBuilder(), alinea);
+    }
+}
