3c8fec37f97119252685b02d8098bb69b35eb916
charboubm <mustapha.charboub@cgi.com>
2018-03-05 11:31:23 +0100

    add title support

---
+++ legaldocml-core/src/main/java/io/legaldocml/business/builder/support/TitleSupport.java
@@ -0,0 +1,27 @@
+package io.legaldocml.business.builder.support;
+
+import io.legaldocml.akn.AknObject;
+import io.legaldocml.akn.container.ANhierContainer;
+import io.legaldocml.akn.element.Title;
+import io.legaldocml.business.builder.element.HierarchyBuilder;
+
+import java.util.function.Consumer;
+
+/**
+ * @author <a href="mailto:mustapha.charboub@gmail.com">Mustapha CHARBPUB</a>
+ */
+public interface TitleSupport<T extends ANhierContainer<E>, E extends AknObject> extends SupportBuilder<T> {
+
+    default HierarchyBuilder<Title> title() {
+        return title(null);
+    }
+
+    default HierarchyBuilder<Title> title(Consumer<Title> consumer) {
+        Title title = new Title();
+        parent().add(title);
+        if (consumer != null) {
+            consumer.accept(title);
+        }
+        return new HierarchyBuilder<>(businessBuilder(), title);
+    }
+}
--- legaldocml-core/src/test/java/io/legaldocml/business/builder/support/AlineaSupportTest.java
+++ legaldocml-core/src/test/java/io/legaldocml/business/builder/support/AlineaSupportTest.java
@@ -1,59 +1,64 @@
 package io.legaldocml.business.builder.support;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.doCallRealMethod;
+
+import java.util.function.Consumer;
+
+import org.junit.jupiter.api.Test;
+
 import io.legaldocml.akn.element.Alinea;
 import io.legaldocml.akn.element.Article;
 import io.legaldocml.akn.element.HierarchyElement;
 import io.legaldocml.akn.element.Point;
 import io.legaldocml.business.builder.element.HierarchyBuilder;
-import org.junit.jupiter.api.Test;
-
-import java.util.function.Consumer;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.nullable;
-import static org.mockito.Mockito.doCallRealMethod;
 
 /**
  * @author <a href="mailto:mustapha.charboub@gmail.com">Mustapha CHARBPUB</a>
  */
+
+@SuppressWarnings("unchecked")
 class AlineaSupportTest extends SupportBuilderTestCase<AlineaSupport<Article, HierarchyElement>, Article> {
 
-    @Test
-    void testEmpty() {
-        doCallRealMethod().when(mock).alinea();
-        doCallRealMethod().when(mock).alinea(nullable(Consumer.class));
-        mock.alinea();
-        assertEquals("<article><alinea/></article>", write());
-    }
+	@Test
+	void testEmpty() {
+		doCallRealMethod().when(mock).alinea();
+		doCallRealMethod().when(mock).alinea(nullable(Consumer.class));
+		mock.alinea();
+		assertEquals("<article><alinea/></article>", write());
+	}
 
-    @Test
-    void testWithNum() {
-        doCallRealMethod().when(mock).alinea();
-        doCallRealMethod().when(mock).alinea(nullable(Consumer.class));
-        mock.alinea().num().text("1");
-        assertEquals("<article><alinea><num>1</num></alinea></article>", write());
-    }
+	@Test
+	void testWithNum() {
+		doCallRealMethod().when(mock).alinea();
+		doCallRealMethod().when(mock).alinea(nullable(Consumer.class));
+		mock.alinea().num().text("1");
+		assertEquals("<article><alinea><num>1</num></alinea></article>", write());
+	}
 
-    @Test
-    void testWithHeading() {
-        doCallRealMethod().when(mock).alinea();
-        doCallRealMethod().when(mock).alinea(nullable(Consumer.class));
-        HierarchyBuilder<Alinea> alineaHierarchyBuilder = mock.alinea();
-        alineaHierarchyBuilder.heading().text("alinea heading");
-        assertEquals("<article><alinea><heading>alinea heading</heading></alinea></article>", write());
-    }
+	@Test
+	void testWithHeading() {
+		doCallRealMethod().when(mock).alinea();
+		doCallRealMethod().when(mock).alinea(nullable(Consumer.class));
+		HierarchyBuilder<Alinea> alineaHierarchyBuilder = mock.alinea();
+		alineaHierarchyBuilder.heading().text("alinea heading");
+		assertEquals("<article><alinea><heading>alinea heading</heading></alinea></article>", write());
+	}
 
-    @Test
-    void testWithPoint() {
-        doCallRealMethod().when(mock).alinea();
-        doCallRealMethod().when(mock).alinea(nullable((Consumer.class)));
-        HierarchyBuilder<Alinea> alineaHierarchyBuilder = mock.alinea();
-        alineaHierarchyBuilder.num().text("1");
-        alineaHierarchyBuilder.heading().text("heading alinea");
-        HierarchyBuilder<Point> pointHierarchyBuilder = alineaHierarchyBuilder.point();
-        pointHierarchyBuilder.num().text("1");
-        pointHierarchyBuilder.heading().text("Point heading");
-        assertEquals("<article><alinea><num>1</num><heading>heading alinea</heading><point><num>1</num><heading>Point heading</heading></point></alinea></article>", write());
-    }
+	@Test
+	void testWithPoint() {
+		doCallRealMethod().when(mock).alinea();
+		doCallRealMethod().when(mock).alinea(nullable((Consumer.class)));
+		HierarchyBuilder<Alinea> alineaHierarchyBuilder = mock.alinea();
+		alineaHierarchyBuilder.num().text("1");
+		alineaHierarchyBuilder.heading().text("heading alinea");
+		HierarchyBuilder<Point> pointHierarchyBuilder = alineaHierarchyBuilder.point();
+		pointHierarchyBuilder.num().text("1");
+		pointHierarchyBuilder.heading().text("Point heading");
+		assertEquals(
+				"<article><alinea><num>1</num><heading>heading alinea</heading><point><num>1</num><heading>Point heading</heading></point></alinea></article>",
+				write());
+	}
 
 }
