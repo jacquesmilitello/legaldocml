a7d0a3b2f46498429c91a33118981021e32bec00
charboubm <mustapha.charboub@gmail.com>
2018-03-28 16:44:55 +0200

    add sub support

--- legaldocml-core/src/main/java/io/legaldocml/business/builder/support/SubSupport.java
+++ legaldocml-core/src/main/java/io/legaldocml/business/builder/support/SubSupport.java
@@ -2,9 +2,26 @@ package io.legaldocml.business.builder.support;
 
 import io.legaldocml.akn.AknObject;
 import io.legaldocml.akn.container.HTMLinlineContainer;
+import io.legaldocml.akn.element.Sub;
+import io.legaldocml.business.builder.element.InlineTypeBuilder;
+
+import java.util.function.Consumer;
 
 /**
  * @author <a href="mailto:mustapha.charboub@gmail.com">Mustapha CHARBOUB</a>
  */
 public interface SubSupport<T extends HTMLinlineContainer<E>, E extends AknObject> extends SupportBuilder<T> {
+    default InlineTypeBuilder<Sub> sub() {
+        return sub(null);
+    }
+
+    default InlineTypeBuilder<Sub> sub(Consumer<Sub> consumer) {
+        Sub sub = new Sub();
+        parent().add(sub);
+        businessBuilder().getContext().push(parent(), sub);
+        if (consumer != null) {
+            consumer.accept(sub);
+        }
+        return new InlineTypeBuilder<>(businessBuilder(), sub);
+    }
 }
