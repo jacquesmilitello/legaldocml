4251cae867a41b6b5866132068034f149447374c
MustaphaCh <charboubmustapha@gmail.com>
2018-04-10 16:25:38 +0200

    add ItemType visitor and fix extranilzable remove issue

--- legaldocml-core/src/main/java/io/legaldocml/akn/visitor/group/ItemTypeVisitor.java
+++ legaldocml-core/src/main/java/io/legaldocml/akn/visitor/group/ItemTypeVisitor.java
@@ -1,4 +1,16 @@
 package io.legaldocml.akn.visitor.group;
 
-public class ItemTypeVisitor {
+import io.legaldocml.akn.element.Item;
+
+/**
+ * @author <a href="mailto:mustapha.charboub@gmail.com">Mustapha CHARBOUB</a>
+ */
+public interface ItemTypeVisitor {
+
+    default boolean visitEnter(Item item) {
+        return true;
+    }
+
+    default void visitLeave(Item item) {
+    }
 }
